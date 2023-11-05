faf29080ef967313f96051cc08da563874b19096
Ellivelton Narciso <ellivelton.souza@gmail.com>
2023-11-05 11:48:12 +0000

    :white_check_mark: test: Adicionado prototipo de testes unitarios.

--- api/tests/functional/hello_world.spec.ts
+++ api/tests/functional/hello_world.spec.ts
@@ -1,7 +1,8 @@
 import { test } from '@japa/runner'
 
+const base :string  = '/api'
 test('display welcome page', async ({ client }) => {
-  const response = await client.get('/')
+  const response = await client.get(`${base}/`)
 
   response.assertStatus(200)
   response.assertBodyContains({ hello: 'world' })
