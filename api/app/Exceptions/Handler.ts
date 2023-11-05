d97f07bed392331f8ccd83e0edc3091dd15c559a
Ellivelton Narciso <ellivelton.souza@gmail.com>
2023-11-05 11:45:28 +0000

    feat: Adicionado rota 404

--- api/app/Exceptions/Handler.ts
+++ api/app/Exceptions/Handler.ts
@@ -15,9 +15,20 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
-  constructor() {
-    super(Logger)
-  }
+    constructor() {
+        super(Logger)
+    }
+
+    public async handle(error: any, ctx: HttpContextContract) {
+        if (error.code === 'E_ROUTE_NOT_FOUND') {
+            return ctx.response.status(404).json({
+                status: 404,
+                msg: 'Rota nao definida.',
+            })
+        }
+        return super.handle(error, ctx)
+    }
 }
