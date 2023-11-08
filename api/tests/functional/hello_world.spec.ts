7d88860ee4e761e9e75d58aa6001d7ce43e6f793
Ellivelton Narciso <ellivelton.souza@gmail.com>
2023-11-08 13:23:37 +0000

    removido

--- api/tests/functional/hello_world.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@japa/runner'
-
-const base :string  = '/api'
-test('display welcome page', async ({ client }) => {
-  const response = await client.get(`${base}/`)
-
-  response.assertStatus(200)
-  response.assertBodyContains({ hello: 'world' })
-})
